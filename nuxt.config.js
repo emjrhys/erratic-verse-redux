c5fa57f992ff3fd5e7884bc0085dba1d8fa0cdc5
noahlafferty <noah.lafferty@gmail.com>
2021-05-07 16:34:46 -0500

    name change

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'erratic verse',
+    title: 'unstable verse',
     htmlAttrs: {
       lang: 'en'
     },
